51fddd2108b9b53b3e9e672a177be1c67f033361
PC <skachat30913@gmail.com>
2024-11-05 23:37:54 +0400

    test

--- pages/create_account_page.py
+++ pages/create_account_page.py
@@ -8,47 +8,39 @@ class CreateAccount(BasePage):
 
     page_url = '/authorization/signup'
 
-    @allure.step('Поле Имя ввод 5 или 32 символов латиницы')
     def field_incorrect_user_name(self, text):
         field = self.page.get_by_label(loc.field_loc)
         field.press_sequentially(text)
         field.press('Enter')
 
-    @allure.step('Поле Имя ввод спецсимволов')
     def field_special_character(self, text):
         field = self.page.get_by_label(loc.field_loc)
         field.press_sequentially(text)
 
-    @allure.step('Поле Имя очистить поле')
     def clear_field_user_name(self):
         field = self.page.get_by_label(loc.field_loc)
         field.click()
         field.press("Control+A")
         field.press("Backspace")
 
-    @allure.step('Проверка ожидаемого результата о вводе некорректных данных ')
     def check_user_error_message(self, text):
         expect(self.page.get_by_text(loc.mes_error)).to_have_text(text)
 
-    @allure.step('Проверка ожидаемого результата о вводе некорректных данных ')
     def check_user_name_error_message(self, text):
         expect(self.page.get_by_text(loc.mess_error)).to_contain_text(text)
 
 
-    @allure.step('Поле email ввод некорректных данных')
     def field_incorrect_email(self, text):
         field = self.page.get_by_label(loc.email_loc)
         field.press_sequentially(text)
         field.press('Enter')
 
-    @allure.step('Поле email очистить поле')
     def clear_field_email(self):
         field = self.page.get_by_label(loc.email_loc)
         field.click()
         field.press("Control+A")
         field.press("Backspace")
 
-    @allure.step('Проверка ожидаемого результата о вводе некорректных данных ')
     def check_email_error_message(self, text):
         expect(self.page.get_by_text(loc.email_error_loc)).to_have_text(text)
 
